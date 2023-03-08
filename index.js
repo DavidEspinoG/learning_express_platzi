6ae61719543496f45cba3b92ae0bb831fdfaa406
David Espino <davidespinog@gmail.com>
2023-03-08 08:25:45 -0600

    Add new endpoints

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ app.get('/', (req, res) => {
   res.send('Hola desde mi primer server en express')
 });
 
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: "Product 1",
+      price: 1
+    },
+    {
+      name: "Product 2",
+      price: 1
+    }
+  ])
+});
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  res.json([
+    {
+      id, 
+      name: "Product 1",
+      price: 1
+    }
+  ])
+});
+
 app.listen(port, () => {
-  console.log('Mi port: ' + port)
+  console.log('Mi puerto: ' + port)
 });
