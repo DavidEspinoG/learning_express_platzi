3d4a657cb20a0a3f9e83bc61ed8bf4322f4f7082
David Espino <davidespinog@gmail.com>
2023-03-08 10:46:49 -0600

    Add post, patch and delete methods to products endpoint

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -16,22 +16,49 @@ router.get('/', (req, res) => {
   res.json(products)
 });
 router.get('/:id', (req, res) => {
-  const { id } = req.params
-  res.json([
-    {
-      id, 
-      name: "Product 1",
-      price: 1
-    }
-  ])
+  const { id } = req.params;
+  if (id === '999') {
+    res.status(404).json([
+      {
+        message: 'not found'
+      }
+    ])
+  } else {
+    res.json([
+      {
+        id, 
+        name: "Product 1",
+        price: 1
+      }
+    ])
+  }
+  
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
-  })
-})
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const body = req.body;
+  const { id } = req.params;
+  res.json({
+    message: 'Partial update',
+    data: body,
+    id
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id
+  });
+});
 
 module.exports = router;
