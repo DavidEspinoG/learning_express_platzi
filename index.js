1554f5494f73dba67cab92260dd77e9c3ca1b592
David Espino <davidespinog@gmail.com>
2023-03-08 15:23:30 -0600

    Add cors configuration

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const routerApi = require('./routes');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
 const { errorHandler, logErrors, boomErrorHandler } = require('./middlewares/errorHandler')
 
 app.use(express.json());
-
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hola desde mi primer server en express')
 });
