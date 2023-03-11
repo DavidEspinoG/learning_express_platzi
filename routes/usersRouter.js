fcf2044cf044a57e6dda19a497b3bc335d95252f
David Espino <davidespinog@gmail.com>
2023-03-10 19:47:21 -0600

    Add postgress.js

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const UserService = require('../services/userService');
+const service = new UserService();
 
-router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset){
-    res.json({
-      limit, 
-      offset
-    })
-  } else {
-    res.send('No hay parámetros')
-  }
+router.get('/', async (req, res) => {
+  const users = await service.find();
+  res.json(users);
 })
 
 module.exports = router;
