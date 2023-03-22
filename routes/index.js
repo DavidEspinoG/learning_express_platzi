dc1ea1679947a44c0cc08b164325aaedbe45a55b
David Espino <davidespinog@gmail.com>
2023-03-22 14:23:10 -0600

    Add Customer table

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const productsRouter = require('./productsRouter');
 const usersRouter = require('./usersRouter');
+const customersRouter = require('./customerRouter');
 const express = require('express');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
-  router.use('/users', usersRouter)
+  router.use('/users', usersRouter);
+  router.use('/customers', customersRouter);
 }
 
 module.exports = routerApi;
